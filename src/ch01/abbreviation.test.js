4f4a6899d91e1c708871ccfbbb9ed407631330df
Gregory <whatifwedigdeeper@gmail.com>
2017-07-22 14:11:58 -0500

    Functional version of abbreviation using lodash/fp

--- src/ch01/abbreviation.test.js
+++ src/ch01/abbreviation.test.js
@@ -1,12 +1,39 @@
+import flow from 'lodash/fp/flow';
+import head from 'lodash/fp/head';
+import tail from 'lodash/fp/tail';
+import compose from 'lodash/fp/compose';
+
+const splitBySpace = (val) => val.split(' ');
+
+const firstInitial = (name) => (
+  flow(splitBySpace, head, head)(name)
+);
+
+const lastName = (name) => (
+  compose(tail, splitBySpace)(name)
+);
+
+const abbrevName = (name) => (
+  `${firstInitial(name)}.${lastName(name)}`
+);
+
+// non-functional style
 const abbreviateName = (name) => {
   const names = name.split(' ');
   const initial = names[0].charAt(0);
   return `${initial}.${names[1]}`;
 };
 
-describe('Abreviation', () => {
+describe('Abbreviation', () => {
   it('should get first letter', () => {
     expect(abbreviateName('Alonzo Church'))
+      .toBe('A.Church');
+  });
+});
+
+describe('Functional Style Abbreviation', () => {
+  it('should get first letter', () => {
+    expect(abbrevName('Alonzo Church'))
       .toBe('A.Church');
   });
 });
