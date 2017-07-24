7799db0820b284140b704629b111ac9f063a801f
WhatIfWeDigDeeper <whatifwedigdeeper@gmail.com>
2017-07-23 20:44:06 -0500

    Added positionOf and valueAt tail recursive functions

--- src/ch04/linkList.test.js
+++ src/ch04/linkList.test.js
@@ -16,6 +16,23 @@ const revReduce = (xs) => (
   ), [])
 );
 
+const positionOf = (xs, x, acc=0) => {
+  if (isEmpty(xs)) {
+    return -1;
+  }
+  return (head(xs) === x)
+    ? acc
+    : positionOf(tail(xs), x, acc + 1);
+};
+
+const valueAt = (xs, i, acc=0) => {
+  if (isEmpty(xs))
+    return null;
+  return (i === acc)
+    ? head(xs)
+    : valueAt(tail(xs), i, acc + 1)
+};
+
 describe('Reverse linked list', () => {
   it('should reverse array', () => {
     expect(reverse([1, 2, 3, 4, 5]))
@@ -27,4 +44,26 @@ describe('Reverse linked list', () => {
       .toEqual([5, 4, 3, 2, 1]);
   });
 
+});
+
+describe('valueAt', () => {
+  it('should return value at position', () =>{
+    expect(valueAt(['a','b','c'], 1))
+      .toBe('b');
+  });
+  it('should return null for index outside', () => {
+    expect(valueAt(['a','b','c'], 4))
+      .toBeNull();
+  })
+});
+
+describe('positionOf', () => {
+  it('should return position of value', () =>{
+    expect(positionOf(['a','b','c'], 'b'))
+      .toEqual(1);
+  });
+  it('should return -1 when element not found', () => {
+    expect(positionOf(['a','b','c'], 'e'))
+      .toEqual(-1);
+  })
 });
