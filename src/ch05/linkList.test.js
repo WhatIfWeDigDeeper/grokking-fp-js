e42c9af4763fa9b92f4c20d67b8c6aede9c85a9c
WhatIfWeDigDeeper <whatifwedigdeeper@gmail.com>
2017-07-23 20:10:22 -0500

    Added reverse function from chapter 5

--- src/ch05/linkList.test.js
+++ src/ch05/linkList.test.js
@@ -2,10 +2,18 @@ import isEmpty from 'lodash/fp/isEmpty';
 import head from 'lodash/fp/head';
 import tail from 'lodash/fp/tail';
 
+// tail recursion
 const reverse = (xs, acc=[]) => (
   isEmpty(xs)
     ? acc
     : reverse(tail(xs), [head(xs)].concat(acc))
+);
+
+// using reduce (fold) fn
+const revReduce = (xs) => (
+  xs.reduce((acc, x) => (
+    [x].concat(acc)
+  ), [])
 );
 
 describe('Reverse linked list', () => {
@@ -13,4 +21,10 @@ describe('Reverse linked list', () => {
     expect(reverse([1, 2, 3, 4, 5]))
       .toEqual([5, 4, 3, 2, 1]);
   });
+
+  it('should reverse array with standard reduce function', () => {
+    expect(revReduce([1, 2, 3, 4, 5]))
+      .toEqual([5, 4, 3, 2, 1]);
+  });
+
 });
